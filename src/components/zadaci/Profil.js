68de805d698929232afa7d420bbd2be25c986136
antoniobirtic-creator <antonio.birtic@gmail.com>
2026-01-19 18:33:42 +0100

    dodane stavke u vjestine i lista

--- src/components/zadaci/Profil.js
+++ src/components/zadaci/Profil.js
@@ -21,7 +21,39 @@ const Profil = () => {
     // Ova komponenta vraća info o korisniku.//
     return (
         <div className="container">
-            <h1>Profil stranica</h1>
+
+            
+
+            <h1>Profil korsinika</h1>
+            <p>Ime: {Korisnik.ime}</p>
+            <p>Prezime: {Korisnik.prezime}</p>
+            <p>Godine: {Korisnik.godine}</p>
+            <p>Vozačka dozvola: {Korisnik.vozackaDozvola ? "Da" : "Ne"}</p>
+            <p>ulica: {Korisnik.adresa.ulica}</p>
+            <p>poštanski broj: {Korisnik.adresa.pbroj}</p>
+            <p>grad: {Korisnik.adresa.grad}</p>
+
+            <div>
+                Vještine:
+                <ul>
+               
+
+
+                    {
+
+
+                    Korisnik.vjestine.map(
+                            (vjestina) => (
+                                <li>{vjestina}</li> 
+                            )
+                        )
+                    }
+
+
+
+                </ul>
+            </div>
+
         </div>
     )
 
